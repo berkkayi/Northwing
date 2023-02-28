ff3873298ec3f9ce5b7f96350aa2967da38eb71a
Berk Kayı <berk.kayi@linktera.com.tr>
2023-02-28 23:27:49 +0300

    Create spring folder pattern and frameworks.

---
+++ src/main/java/com/berkkayi/northwind/api/controller/ProductsController.java
@@ -0,0 +1,28 @@
+package com.berkkayi.northwind.api.controller;
+
+import com.berkkayi.northwind.business.abstracts.ProductService;
+import com.berkkayi.northwind.business.concretes.ProductManager;
+import com.berkkayi.northwind.entities.concretes.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductsController {
+
+    private ProductService productService;
+
+    @Autowired
+    public ProductsController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/getall")
+    public List<Product> getAll(){
+       return this.productService.getAll();
+    }
+}
